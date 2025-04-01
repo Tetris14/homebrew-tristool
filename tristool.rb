61ce8d15267ca5f94d3fb9e3d45301aaeafc5e26
tristan gory <gory.tristan92@gmail.com>
2025-04-01 14:46:32 +0200

    fix: update version to 1.1.0 for Tristool CLI tool

--- tristool.rb
+++ tristool.rb
@@ -3,7 +3,7 @@ class Tristool < Formula
   homepage "https://github.com/Tetris14/tristool"
   url "https://github.com/Tetris14/tristool/releases/download/v1.1.0/tristool"
   sha256 "fe12aa8042449529c0accf059df57a04e5c5b1079b3a64e33334839ae7c1925a"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
 
   def install
