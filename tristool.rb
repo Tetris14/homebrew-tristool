31a2a9db4606a2985088a98086bb82f439db757f
tristan gory <gory.tristan92@gmail.com>
2025-04-01 14:37:42 +0200

    fix: update sha256 checksum for Tristool CLI tool

--- tristool.rb
+++ tristool.rb
@@ -2,7 +2,7 @@ class Tristool < Formula
   desc "A CLI tool by Tristanus"
   homepage "https://github.com/Tetris14/tristool"
   url "https://github.com/Tetris14/tristool/releases/download/v1.0.0/tristool"
-  sha256 "5be4c9442a66fd59a5662f0ab49f207056707faa3f2eacfc11dd24dab3a46d2d"
+  sha256 "fe12aa8042449529c0accf059df57a04e5c5b1079b3a64e33334839ae7c1925a"
   version "1.0.0"
   license "MIT"
 
