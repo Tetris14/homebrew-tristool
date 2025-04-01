4850f0ca85c8f49331026b743b226c3d27154630
tristan gory <gory.tristan92@gmail.com>
2025-04-01 10:13:15 +0200

    feat: initial formula for Tristool CLI tool

---
+++ tristool.rb
@@ -0,0 +1,16 @@
+class Tristool < Formula
+  desc "A CLI tool by Tristanus"
+  homepage "https://github.com/Tetris14/tristool"
+  url "https://github.com/Tetris14/tristool/releases/tag/v1.0.0"
+  sha256 "5be4c9442a66fd59a5662f0ab49f207056707faa3f2eacfc11dd24dab3a46d2d"
+  version "1.0.0"
+  license "MIT"
+
+  def install
+    bin.install "tristool"
+  end
+
+  test do
+    system "#{bin}/tristool", "--version"
+  end
+end
