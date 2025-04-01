3be12f0ba012bbd64a1fa982e7b3fdb60334ab06
tristan gory <gory.tristan92@gmail.com>
2025-04-01 10:30:44 +0200

    fix: update download URL for Tristool CLI tool

--- tristool.rb
+++ tristool.rb
@@ -1,7 +1,7 @@
 class Tristool < Formula
   desc "A CLI tool by Tristanus"
   homepage "https://github.com/Tetris14/tristool"
-  url "https://github.com/Tetris14/tristool/releases/tag/v1.0.0"
+  url "https://github.com/Tetris14/tristool/releases/download/v1.0.0/tristool"
   sha256 "5be4c9442a66fd59a5662f0ab49f207056707faa3f2eacfc11dd24dab3a46d2d"
   version "1.0.0"
   license "MIT"
